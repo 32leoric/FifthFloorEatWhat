3eedc694ef12663d8ab588e72f1692e0d704ca64
CCXXXI <ccxxxi@qq.com>
2022-05-02 11:35:17 +0800

    fix: replace "：" with ":"
    这代码之前竟然可以运行

--- gacha.py
+++ gacha.py
@@ -80,7 +80,7 @@ class Wills:
 
         for row in range(0, len(names)):
             if dates[row] == date and names[row] in self.students:
-                if self.wills[names[row]] != []：
+                if self.wills[names[row]] != []:
                     self.wills[names[row]] = []
                 self.wills[names[row]].append(first[row])
                 self.wills[names[row]].append(second[row])
